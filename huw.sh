d8aca2ebe6ae6ebf140109b67cabef106d269a67
frank <frank.vanleijsen@student.hu.nl>
2023-03-30 15:18:04 +0200

    Branch commit

--- huw.sh
+++ huw.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 export FLASK_APP=huw.py
 python -m flask run
+# python -m flask --app huw run --port 5000
--- huw_recommend.sh
+++ huw_recommend.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 export FLASK_APP=huw_recommend.py
 python -m flask run --port 5001
+# python -m flask --app huw_recommend run --port 5001

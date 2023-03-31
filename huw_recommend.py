9cbe411a4249f5206a444bd3777d6c8c2f95ff43
frank <frank.vanleijsen@student.hu.nl>
2023-03-31 10:18:58 +0200

    Made most recommend types

--- huw_recommend.py
+++ huw_recommend.py
@@ -13,12 +13,11 @@ api = Api(app)
 
 # We define these variables to (optionally) connect to an external PostgreSQL
 # instance.
-envvals = ["PGUSER","PGPASSWORD","PGHOST","PGPORT","PGDATABASE"]
+envvals = ["PGUSER", "PGPASSWORD", "PGHOST", "PGPORT", "PGDATABASE"]
 dbstring = 'dbname={4} user={0} password={1} host={2} port={3}'
 
 # Since we are asked to pass a class rather than an instance of the class to the
-# add_resource method, we open the connection to the database outside the
-# Recom class.
+# add_resource method, we open the connection to the database outside the Recom class.
 load_dotenv()
 if os.getenv(envvals[0]) is not None:
     envvals = list(map(lambda x: str(os.getenv(x)), envvals))
@@ -32,21 +31,90 @@ class Recom(Resource):
     the webshop. At the moment, the API simply returns a random set of products
     to recommend."""
 
-    def get(self, profileid, categories, rtype,count):
+    def get(self, profileid, categories, rtype, count):
         """ This function represents the handler for GET requests coming in
         through the API. It currently returns a random sample of products. """
         cursor = conn.cursor()
         if categories == 'None':
-            cursor.execute('''SELECT product_id 
-                              FROM product 
-                              WHERE recommendable = True 
-                              ORDER BY RANDOM() 
-                              LIMIT %s;''', (count,))
+            cursor.execute(f'''SELECT product_id 
+                               FROM product 
+                               WHERE recommendable = True 
+                               ORDER BY RANDOM()
+                               LIMIT {count};
+            ''')
         else:
-            cursor.execute('''SELECT product_id 
-                              FROM product 
-                              ORDER BY random() 
-                              LIMIT %s;''', (count,))  # change this
+            category_name, category_number = categories.split('@')
+            # Add 'sub_' category_number-1 times before adding category
+            category_type = ''.join('sub_' for i in range(0, category_number-1)) + 'category'
+            match rtype:
+                # Anderen kochten ook
+                case 'popular':
+                    cursor.execute(f'''SELECT orders.productproduct_id, COUNT(orders.productproduct_id)
+                                       FROM orders
+                                       JOIN product on orders.productproduct_id = product.product_id
+                                       WHERE recommendable = True AND {category_type} = {category_name}
+                                       GROUP BY orders.productproduct_id
+                                       ORDER BY count DESC
+                                       LIMIT {count};
+                    ''')
+                # Soortgelijke producten
+                case 'similar':
+                    # cursor.execute(f'''SELECT product_id
+                    #                    FROM product
+                    #                    WHERE product_id = {pro}
+                    # ''')
+                    # cursor.execute(f'''SELECT product_id
+                    #                    FROM product
+                    #                    WHERE recommendable = True
+                    #                    ORDER BY CASE WHEN brand = 'Aquafresh' THEN 1
+                 	# 	                             ELSE 2
+					# 	                             END
+                    # ''', (count,)) TODO: brand/product_id doorgeven
+                    cursor.execute(f'''SELECT product_id
+                                       FROM product
+                                       WHERE recommendable = True AND {category_type} = {category_name}
+                                       ORDER BY RANDOM()
+                                       LIMIT {count};
+                    ''')
+                # Combineert goed met
+                case 'combination':
+                    cursor.execute(f'''SELECT ord.sessionssession_id
+                                       FROM orders
+                    ''')
+                # Passend bij uw gedrag
+                case 'behaviour':
+                    cursor.execute(f'''SELECT ev.event_product
+                                       FROM profile prof
+                                       JOIN identifier AS iden ON prof.profile_id = iden.profileprofile_id
+                                       JOIN sessions AS ses ON iden.bu_id = ses.bu_id
+                                       JOIN event AS ev ON ses.session_id = ev.sessionssession_id
+                                       JOIN product AS prod ON ev.event_product = prod.product_id
+                                       WHERE prod.recommendable = True AND prof.profile_id = {profileid}
+                                       LIMIT {count};
+                    ''') # TODO: Minimaal 4 garanderen
+                # Persoonlijk aanbevolen
+                case 'personal':
+                    cursor.execute(f'''SELECT preference_type, preference_name
+                                       FROM profile prof
+                                       JOIN identifier AS iden ON prof.profile_id = iden.profileprofile_id
+                                       JOIN sessions AS ses ON iden.bu_id = ses.bu_id
+                                       JOIN preferences AS pref ON ses.session_id = pref.Sessionssession_id
+                                       WHERE prof.profile_id = {profileid}
+                                       LIMIT {count};
+                    ''')
+                    preferences = cursor.fetchall()
+                    preferred = ''''''
+                    for preference in preferences:
+                        preferred += f'''OR {preference[0]} = '{preference[1]}' '''
+                    if preferred != '''''':
+                        preferred = '''AND ''' + preferred
+                    cursor.execute(f'''SELECT product_id
+                                       FROM product
+                                       WHERE recommendable = True AND (folder_active = 'Enabled' 
+                                            OR discount IS NOT Null)
+                                            {preferred}
+                                       LIMIT {count};
+                    ''')
         prodids = [row[0] for row in cursor.fetchall()]
         cursor.close()
         print(categories)
@@ -56,4 +124,4 @@ class Recom(Resource):
 
 # This method binds the Recom class to the REST API, to parse specifically
 # requests in the format described below.
-api.add_resource(Recom, "/<string:profileid>/<string:categorys>/<string:rtype>/<int:count>")
+api.add_resource(Recom, "/<string:profileid>/<string:categories>/<string:rtype>/<int:count>")
